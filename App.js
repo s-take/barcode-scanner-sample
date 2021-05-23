fae6234ac9f4e94bde2d37cf499bdf432f8facca
s-take <chantake33@gmail.com>
2021-05-24 08:58:46 +0900

    commit

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Container, Text, Button, Icon } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import BarcodeScannerScreen from "./components/BarcodeScannerScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>BarCode Scanner Sample{"\n"}</Text>
+        <Button
+          style={{ alignSelf: "center" }}
+          onPress={() => this.props.navigation.navigate("バーコード読取")}
+          >
+          {/* <Icon type="FontAwesome5" name="barcode" /> */}
+          <Text>バーコード読取</Text>
+          </Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>バーコード読取</Text>
+      </View>
+    );
+  }
+}
+
+const Stack = createStackNavigator();
+function RootStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ gestureEnabled: false }}
+    >
+      <Stack.Screen name="ホーム" component={HomeScreen} />
+      <Stack.Screen name="詳細" component={DetailsScreen} />
+      <Stack.Screen name="バーコード読取" component={BarcodeScannerScreen} />
+    </Stack.Navigator>
+  );
+}
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <RootStack />
+//     </NavigationContainer>
+//   );
+// }
+
+export default class App extends React.Component {
+  // ロードが終わるまでは「loading...」を表示するため、state「isReady」で制御
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false
+    };
+  }
+  // DidMountのタイミングでフォントリソースをメモリ上に読み込み。終わったらisReadyをオン。
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
+    });
+    this.setState({ isReady: true });
+  }
+  render() {
+    //Wait for font loading... フォントの読み込み中なら、「loading...」を表示
+    if (!this.state.isReady) {
+      return (
+        <View>
+          <Text>loading...</Text>
+        </View>
+      );
+    }
+
+    return (
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+    );
+  }
+}
---
+++ components/BarcodeScannerScreen.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from "react-native";
+
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+// class BarcodeScannerScreenContent extends React.Component {
+export default function BarcodeScannerScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`バーコードタイプ；${type} \n読取結果：${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'タップして再スキャン'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
+  });
+
+// const BarcodeScannerScreen = ({ navigation }) => {
+//   return (
+//     <Subscribe to={[InvoiceContainer]}>
+//       {globalState => <BarcodeScannerScreenContent globalState={globalState} navigation={navigation} />}
+//     </Subscribe>
+//   );
+// };
